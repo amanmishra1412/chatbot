5e0ada2bc54a4d307319e96985183a349e1eee0e
amanmishra1412 <mishra06aman@gmail.com>
2026-07-07 14:44:21 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("hello");
-});
-
 app.post("/chat", async (req, res) => {
     try {
         const { message, provider } = req.body;
